2764aa5018a293ccf94c27a5ba2865c384fe2197
alex-govier5 <alex.d.g@icloud.com>
2024-11-17 18:58:14 -0500

    Update notebooks and comment out new functions

--- src/utils/bump.py
+++ src/utils/bump.py
@@ -25,6 +25,23 @@ def collect_bumps(dataset, phi, threshold=0.8):
 
     return bumps
 
+# NEW
+# def collect_bumps(dataset, phi, threshold=0.8):
+#     bumps = []
+#     n_samples = dataset.count()
+#     i_start = 0
+#     current_type = "normal" if phi[0] < threshold else "rare"
+
+#     for i in range(1, n_samples):
+#         is_rare = phi[i] >= threshold
+#         if (is_rare and current_type == "normal") or (not is_rare and current_type == "rare"):
+#             bumps.append(Bump(current_type, select_df(dataset, i_start, i - 1)))
+#             i_start = i
+#             current_type = "rare" if is_rare else "normal"
+
+#     bumps.append(Bump(current_type, select_df(dataset, i_start, n_samples - 1)))
+
+#     return bumps
 
 def get_rare_bumps(bumps):
     return [bump for bump in bumps if bump.type == "rare"]
